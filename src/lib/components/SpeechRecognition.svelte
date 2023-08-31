4f57f0f8ac1ba629b8f77bb8885e9c8c35595fa7
Kin <withcenter.dev4@gmail.com>
2023-08-31 14:40:53 +0800

    cloning code pen code

---
+++ src/lib/components/SpeechRecognition.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+</script>
+
+<section>
+	<h1>Speech<br /> Recognition</h1>
+	<p>Available In Chrome😎 Only</p>
+	<div class="container">
+		<div class="texts" />
+	</div>
+</section>
+
+<style>
+	section {
+		min-height: 100vh;
+		width: 100%;
+		display: flex;
+		align-items: flex-start;
+		background-color: rgb(37, 37, 37);
+		flex-direction: column;
+		padding: 50px 0;
+	}
+	section h1 {
+		color: rgba(255, 255, 255, 0.322);
+		text-align: center;
+		width: 100%;
+		font-size: 50px;
+		margin-bottom: 10px;
+	}
+	section p {
+		text-align: center;
+		color: rgba(255, 255, 255, 0.322);
+		width: 100%;
+		margin-bottom: 40px;
+	}
+	.container {
+		width: 90%;
+		max-width: 500px;
+		margin: 0 auto;
+		justify-content: center;
+	}
+	.texts p {
+		color: black;
+		text-align: left;
+		width: 100%;
+		background-color: white;
+		padding: 10px;
+		border-radius: 8px;
+		margin-bottom: 10px;
+	}
+	.texts p .replay {
+		text-align: right;
+	}
+</style>
